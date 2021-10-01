rob006/yii

commit 3881dddc6eb6ca8ea43a76797bf65f24088a8abe

Fixed: PHP 7.2 each() function in CTabView is deprecated #4199 (#4209)
- Fixed each usages in CTabView.php
- Fixed each usages in native.php

framework/gii/components/Pear/Text/Diff/Engine/native.php
@@ -192,7 +192,9 @@ class Text_Diff_Engine_native {
                 }
                 $matches = $ymatches[$line];
                 reset($matches);
-                while (list(, $y) = each($matches)) {
+                foreach($matches as $match) {
+                    reset($match);
+                    $y = next($match);
                     if (empty($this->in_seq[$y])) {
                         $k = $this->_lcsPos($y);
                         assert($k > 0);
@@ -200,7 +202,9 @@ class Text_Diff_Engine_native {
                         break;
                     }
                 }
-                while (list(, $y) = each($matches)) {
+                foreach($matches as $match) {
+                    reset($match);
+                    $y = next($match);
                     if ($y > $this->seq[$k - 1]) {
                         assert($y <= $this->seq[$k]);
                         /* Optimization: this is a common case: next match is

framework/web/widgets/CTabView.php
@@ -131,7 +131,7 @@ class CTabView extends CWidget
 		if($this->activeTab===null || !isset($this->tabs[$this->activeTab]))
 		{
 			reset($this->tabs);
-			list($this->activeTab, )=each($this->tabs);
+			$this->activeTab = current($this->tabs);
 		}
 
 		$htmlOptions=$this->htmlOptions;

commit 3de9462697b97c2f90a57228f8e3c0c16d278c84

Apply proper refactor for deprecated each(), broken since https://github.com/yiisoft/yii/commit/3881dddc6eb6ca8ea43a76797bf65f24088a8abe

framework/gii/components/Pear/Text/Diff/Engine/native.php
@@ -191,10 +191,7 @@ class Text_Diff_Engine_native {
                     continue;
                 }
                 $matches = $ymatches[$line];
-                reset($matches);
-                foreach($matches as $match) {
-                    reset($match);
-                    $y = next($match);
+                foreach($matches as $y) {
                     if (empty($this->in_seq[$y])) {
                         $k = $this->_lcsPos($y);
                         assert($k > 0);
@@ -202,9 +199,7 @@ class Text_Diff_Engine_native {
                         break;
                     }
                 }
-                foreach($matches as $match) {
-                    reset($match);
-                    $y = next($match);
+                foreach($matches as $y) {
                     if ($y > $this->seq[$k - 1]) {
                         assert($y <= $this->seq[$k]);
                         /* Optimization: this is a common case: next match is